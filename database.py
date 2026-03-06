43c8d1da6999fa7c321ed342544dc319492f8980
Paolo Serhal <126347977+paolosrhl@users.noreply.github.com>
2026-03-06 13:03:12 +0100

    changes

--- database.py
+++ database.py
@@ -1,9 +1,22 @@
+import os
 import mysql.connector
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 def get_connection():
+    host = os.getenv("DB_HOST")
+    user = os.getenv("DB_USER")
+    password = os.getenv("DB_PASSWORD")
+    database = os.getenv("DB_NAME")
+
+    if not host or not user or not password or not database:
+        raise ValueError("One or more database environment variables are missing.")
+
     return mysql.connector.connect(
-        host="YOUR_HOST",
-        user="YOUR_USER",
-        password="YOUR_PASSWORD",
-        database="YOUR_DATABASE"
+        host=host,
+        user=user,
+        password=password,
+        database=database
     )
